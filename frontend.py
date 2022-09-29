efa9bed2f6e3aa8b5fc93289fa43081ab053ad25
someshfengde <someshfengde@gmail.com>
2022-09-30 01:33:06 +0530

    removing unnecesary

--- frontend.py
+++ frontend.py
@@ -7,4 +7,4 @@ buffer = st.file_uploader('upload image file here', type=['jpg', 'png', 'jpeg'])
 if buffer is not None:
   st.write('loading')
   image, name, accuracy = predict_and_view(buffer, './effnet_model.pth')
-  st.image(image, caption=f'prediction is {name} with accuracy {accuracy}')
+  st.image(image, caption=f'prediction is {name}')
