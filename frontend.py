2db9a9e6eb21cbef10c8624419c2ffb2954d7253
Omkar Chougule <63452279+Omichougule@users.noreply.github.com>
2022-09-30 00:52:05 +0530

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,2 +1,5 @@
 import streamlit as st
 import panda as pd
+
+
+st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
